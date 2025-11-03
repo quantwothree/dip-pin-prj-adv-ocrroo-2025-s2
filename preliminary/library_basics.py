1362f12f6ca0d2c7c938daaac55f37d04156a93a
Quan Mai <20136769@tafe.wa.edu.au>
2025-11-03 16:51:39 +0800

    feat: add OCR function

--- preliminary/library_basics.py
+++ preliminary/library_basics.py
@@ -16,10 +16,13 @@ from pathlib import Path
 from PIL import Image
 import cv2
 import numpy as np
+import tesserocr
 
 
 
 VID_PATH = Path("../resources/oop.mp4")
+TESSDATA_FOLDER = "../resources/"
+IMAGE_PATH = Path("../resources/input.png")
 
 class CodingVideo:
     capture: cv2.VideoCapture
@@ -79,11 +82,44 @@ class CodingVideo:
         image = Image.fromarray(rgb_array)
         image.save(output_path)
 
+    def get_text_from_time(self, seconds: int) -> str:
+
+        frame_number = self.get_frame_number_at_time(seconds)
+        rgb_array = self.get_frame_rgb_array(frame_number)
+        image = Image.fromarray(rgb_array)
+
+        # PyTessBaseAPI takes the folder containing the tessdata file as 'path' and also the language as 'lang'
+        api = tesserocr.PyTessBaseAPI(path=TESSDATA_FOLDER, lang='eng')
+        api.SetImage(image)
+        return api.GetUTF8Text()
+
+    def get_text_from_frame(self, frame: int) -> str:
+
+        rgb_array = self.get_frame_rgb_array(frame)
+        image = Image.fromarray(rgb_array)
+
+        # PyTessBaseAPI takes the folder containing the tessdata file as 'path' and also the language as 'lang'
+        api = tesserocr.PyTessBaseAPI(path=TESSDATA_FOLDER, lang='eng')
+        api.SetImage(image)
+        return api.GetUTF8Text()
+
+    def get_text_from_image(self, image: Path | str) -> str:
+
+        api = tesserocr.PyTessBaseAPI(path=TESSDATA_FOLDER, lang='eng')
+        api.SetImageFile(str(image))
+        return api.GetUTF8Text()
+
 def test():
 
-    oop = CodingVideo("../resources/oop.mp4")
+    oop = CodingVideo(VID_PATH)
     print(oop)
     oop.save_as_image(42)
+    text_from_time= oop.get_text_from_time(42)
+    text_from_frame = oop.get_text_from_frame(1007)
+    text_from_image = oop.get_text_from_image(IMAGE_PATH)
+    print(text_from_time)
+    print(text_from_frame)
+    print(text_from_image)
 
 if __name__ == '__main__':
     test()
